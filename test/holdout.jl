5400d6b77fe4fd4302baf24fb2d20be5796f23d3
StevenWhitaker <steventwhitaker@gmail.com>
2020-03-02 13:53:44 -0500

    Add test.

--- test/holdout.jl
+++ test/holdout.jl
@@ -23,8 +23,32 @@ function test_holdout_1()
 
 end
 
+function test_holdout_2()
+
+    Random.seed!(0)
+    N = 10
+    T = 200
+    λvals = [1, 2]
+    ρvals = [1, 2]
+    weights = [1]
+    xDistsTest = [Uniform(100, 100.000000001)]
+    νDistsTest = Vector{Vector{Int}}()
+    xDistsTrain = [Uniform(10, 500)]
+    νDistsTrain = Vector{Vector{Int}}()
+    noiseDist = Normal(0, 0.01)
+    signalModels = [x -> exp(-30 / x)]
+    kernelgenerator = Λ -> GaussianKernel(Λ)
+    (λ, ρ, Ψ) = PERK.holdout(N, T, λvals, ρvals, weights, xDistsTest,
+                             νDistsTest, xDistsTrain, νDistsTrain, noiseDist,
+                             signalModels, kernelgenerator, showprogress = true)
+    @show Ψ
+    return λ == 2 && ρ == 1
+
+end
+
 @testset "Holdout" begin
 
     @test test_holdout_1()
+    @test test_holdout_2()
 
 end
