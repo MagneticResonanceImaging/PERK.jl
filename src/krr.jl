fd7cebb0058b5dede6b54977707c32260c3af340
StevenWhitaker <steventwhitaker@gmail.com>
2020-03-02 21:07:33 -0500

    Flesh out krr.jl.

--- src/krr.jl
+++ src/krr.jl
@@ -4,31 +4,150 @@
 Train kernel ridge regression.
 
 # Arguments
-- `xtrain::AbstractVector{<:Union{<:Real,<:AbstractVector{<:Real}}}`: Latent
-  parameters for training data [T]; each element of `xtrain` has length L for
-  the number of latent parameters
-- `ytrain::AbstractVector{<:Union{<:Real,<:AbstractVector{<:Real}}}`: Feature
-  vectors for training data [T]; each element of `ytrain` has length Q for the
-  number of features
+- `xtrain::Union{<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}}`: Latent
+  parameters for training data [L,T] or [T] (when L = 1)
+- `ytrain::Union{<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}}`: Features
+  for training data [Q,T] or [T] (when Q = 1)
 - `kernel::Kernel`: Kernel to use
-- `f::AbstractMatrix{<:Real} = randn(kernel.H, Q)`: TODO: Make this a vector?
+- `f::Union{<:AbstractVector{<:Real},AbstractMatrix{<:Real}} = randn(kernel.H, Q)`:
+  Unscaled random frequency values [H,Q] or [H] (when Q = 1) (used when
+  `kernel isa RFFKernel`)
+- `phase::AbstractVector{<:Real} = rand(kernel.H)`: Random phase values [H]
+  (used when `kernel isa RFFKernel`)
+
+## Note
+- L is the number of unknown or latent parameters to be predicted
+- Q is the number of observed features per training sample
+- T is the number of training samples
+- H is approximation order for kernels that use random Fourier features
+
+# Return
+- `trainData::TrainingData`: `TrainingData` object to be passed to `krr`
 """
-function krr_train()
+function krr_train(
+    xtrain::Union{<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}},
+    ytrain::Union{<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}},
+    kernel::ExactKernel
+)
 
+    # TODO: Don't make both matrices, instead dispatch and allow different TrainingData objects
+    # Make both xtrain and ytrain matrices
+    # This should be type stable, assuming permutedims is type stable
+    isa(xtrain, AbstractVector) && (xtrain = permutedims(xtrain)) # [L=1,T]
+    isa(ytrain, AbstractVector) && (ytrain = permutedims(ytrain)) # [Q=1,T]
 
+    # Evaluate the kernel on the training features
+    K = kernel(ytrain, ytrain) # [T,T]
+
+    # Calculate the sample mean and de-mean the latent parameters
+    xm = dropdims(mean(xtrain, dims = 2), dims = 2) # [L]
+    x = x .- xm # [L,T]
+
+    # De-mean the rows and columns of the kernel output
+    Km = dropdims(mean(K, dims = 2), dims = 2) # [T]
+    K = K .- Km # [T,T]
+    K = K .- mean(K, dims = 1) # [T,T]
+
+    return ExactTrainingData(q, x, xm, K, Km)
+
+end
+
+function krr_train(
+    xtrain::Union{<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}},
+    ytrain::Union{<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}},
+    kernel::RFFKernel,
+    f::Union{<:AbstractVector{<:Real},<:AbstractMatrix{<:Real},Nothing} = nothing,
+    phase::Union{<:AbstractVector{<:Real},Nothing} = nothing
+)
+
+    # Make xtrain, ytrain, and f matrices
+    isa(xtrain, AbstractVector) && (xtrain = permutedims(xtrain)) # [L=1,T]
+    isa(ytrain, AbstractVector) && (ytrain = permutedims(ytrain)) # [Q=1,T]
+    isa(f, AbstractVector) && (f = reshape(f, :, 1)) # [H,Q=1]
+
+    # Use random Fourier features to approximate the kernel
+    if isnothing(f) || isnothing(phase)
+        (z, freq, phase) = kernel(ytrain)
+    else
+        (z, freq, phase) = kernel(ytrain, f, phase)
+    end
+
+    return _krr_train(xtrain, z, freq, phase)
+
+end
+
+function _krr_train(
+    xtrain::AbstractMatrix{<:Real}, # [L,T]
+    z::AbstractMatrix{<:Real}, # [H,T]
+    freq::AbstractMatrix{<:Real}, # [H,Q]
+    phase::AbstractVector{<:Real} # [H]
+)
+
+    # Grab the number of training points
+    T = size(z, 2)
+
+    # Calculate sample means
+    xm = dropdims(mean(xtrain, dims = 2), dims = 2) # [L]
+    zm = dropdims(mean(z, dims = 2), dims = 2) # [H]
+
+    # Calculate sample covariances
+    x = x .- xm # [L,T]
+    z = z .- zm # [H,T]
+    Czz = div0.(z * z', T) # [H,H]
+    Cxz = div0.(x * z', T) # [L,H]
+
+    return RFFTrainingData(freq, phase, zm, xm, Czz, Cxz)
 
 end
 
 """
-    krr(ytest, ytrain, xtrain, kernel, ρ)
+    krr(ytest, trainData, kernel, ρ)
 
 Predict latent parameters that generated `ytest` using kernel ridge regression.
 
 # Arguments
-- `ytest::Union{<:Real,<:AbstractVector{<:Real}}`: Observed test data
+- `ytest::Union{<:Real,<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}}`:
+  Observed test data [Q,N] or [Q] (if N = 1) or scalar (if Q = N = 1)
+- `trainData::TrainingData`: Training data
+- `kernel::Kernel`: Kernel to use
+- `ρ::Real`: Tikhonov regularization parameter
+
+## Notes
+- Q is the number of observed features per test sample
+- N is the number of test samples
+
+# Return
+- `xhat::Union{<:Real,<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}}`:
+  Estimated latent parameters [L,N] or [L] (if N = 1) or scalar (if L = N = 1)
 """
-function krr()
+function krr(
+    ytest::Union{<:Real,<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}},
+    trainData::ExactTrainingData,
+    kernel::ExactKernel,
+    ρ::Real
+)
 
+    k = kernel(trainData.y, ytest) # [T,N] or [T]
+    k = k .- trainData.Km # [T,N] or [T]
+    tmp = (trainData.K + trainData.T * ρ * I) \ k # [T,N] or [T]
+    xhat = trainData.xm .+ trainData.x * tmp # [L,N] or [L] or scalar TODO: Modify TrainingData to allow for scalars
 
+    return xhat
+
+end
+
+function krr(
+    ytest::Union{<:Real,<:AbstractVector{<:Real},<:AbstractMatrix{<:Real}},
+    trainData::RFFTrainingData,
+    ::ExactKernel,
+    ρ::Real
+)
+
+    z = rffmap(ytest, trainData.freq, trainData.phase) # [H,N] or [H]
+    z = z .- trainData.zm # [H,N] or [H]
+    tmp = (trainData.Czz + ρ * I) \ z # [H,N] or [H]
+    xhat = trainData.xm .+ trainData.Cxz * tmp # [L,N] or [L] or scalar TODO: See above
+
+    return xhat
 
 end
